68509579c0c9a58ca3fffc407558fe906d7f0aa6
Georg Kullgren <georg@kullgren.org>
2013-09-30 18:09:13 +0200

    Added generic math operation that parses the string for add/subtract/divide/multiply keywords.

--- src/StringCalculator/core.clj
+++ src/StringCalculator/core.clj
@@ -45,3 +45,37 @@
   ([] 0)
   ([argString]
      (math-operation argString (fn [a b] (* a b)))))
+
+(defn parse-math
+  [stringArg]
+  (def matched-group (re-find (re-pattern "\\(([a-z0-9\\s]*)\\)") stringArg))
+  (cond 
+   (= matched-group nil) nil
+   :else (get matched-group 1)))
+
+(defn do-operation
+  [expression]
+  (def expression-parts (re-find #"(\w+)\s+(\d+\s+\d+)" expression))
+  (def operation (get expression-parts 1))
+  (def arguments (get expression-parts 2))
+  (cond
+   (= operation "add") (add arguments)
+   (= operation "subtract") (subtract arguments)
+   (= operation "divide") (divide arguments)
+   (= operation "multiply") (multiply arguments)))
+
+
+(defn math
+  [stringArg]
+  (def expression (parse-math stringArg))
+  (cond
+   (= expression nil) 0
+   :else (do-operation expression)))
+
+
+
+;if ( create node
+;if ) calculate result of node
+;iterate
+;
+;(add 3 (add(4 4))
